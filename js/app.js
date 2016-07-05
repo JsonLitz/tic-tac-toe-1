f9b436f456a717957c75b9f3b006bb2680391eab
JSONLITZ <JASONDOTLEE@GMAIL.COM>
2016-07-04 23:24:25 -0700

    add reset button functionality

--- js/app.js
+++ js/app.js
@@ -1,31 +1,71 @@
-
-          // wait for the DOM to finish loading
-$(document).ready(function() {
-
-
 // ============================================================================================================
 // INSIDE THIS IS WHERE STUFF STILL WORKS
-      // var board = 0;
-      // $('.row').find('.box').click(function() {
-      //     if (board % 2 === 0) {
-      //       $(this).prepend('<img class="img X" src="images/oscarO.png" />');
-      //
-      //     } else {
-      //       $(this).prepend('<img class="img O" src="images/xhibit.png" />');
-      //
-      //     }
-      //     board++;
-      //
-      //     var space = $(this).val();
-      //     console.log("code is working !")
-      //
-      //   });
-      //
+// $(document).ready(function() {
+// var board = 0;
+//    $('.row').find('.box').click(function() {
+//          if ($(this).hasClass('O') || $(this).hasClass('X')) {
+//            return false;
+//          }
+//          if (board % 2 === 0) {
+//            $(this).val('x');
+//            $(this).addClass('X');
+//
+//          } else {
+//            $(this).addClass('O');
+//            $(this).val('o');
+//
+//          }
+//          board++;
+//
+//          // console.log("code is working !")
+//
+//          // function that checks if X is the winner
+//          if ($('.one').val() === 'x' && $('.two').val() === 'x' && $('.three').val() === 'x') {
+//            alert('X Wins!');
+//          } else if ($('.four').val() === 'x' && $('.five').val() === 'x' && $('.six').val() === 'x') {
+//            alert('X Wins!');
+//          } else if ($('.seven').val() === 'x' && $('.eight').val() === 'x' && $('.nine').val() === 'x') {
+//            alert('X Wins!');
+//          } else if ($('.one').val() === 'x' && $('.four').val() === 'x' && $('.seven').val() === 'x') {
+//            alert('X Wins!');
+//          } else if ($('.two').val() === 'x' && $('.five').val() === 'x' && $('.eight').val() === 'x') {
+//            alert('X Wins!');
+//          } else if ($('.three').val() === 'x' && $('.six').val() === 'x' && $('.nine').val() === 'x') {
+//            alert('X Wins!');
+//          } else if ($('.one').val() === 'x' && $('.five').val() === 'x' && $('.nine').val() === 'x') {
+//            alert('X Wins!');
+//          } else if ($('.three').val() === 'x' && $('.five').val() === 'x' && $('.seven').val() === 'x') {
+//            alert('X Wins!');
+//          }
+//          //function that checks if O is the winner
+//          else if ($('.one').val() === 'o' && $('.two').val() === 'o' && $('.three').val() === 'o') {
+//            alert('O Wins!');
+//          } else if ($('.four').val() === 'o' && $('.five').val() === 'o' && $('.six').val() === 'o') {
+//            alert('O Wins!');
+//          } else if ($('.seven').val() === 'o' && $('.eight').val() === 'o' && $('.nine').val() === 'o') {
+//            alert('O Wins!');
+//          } else if ($('.one').val() === 'o' && $('.four').val() === 'o' && $('.seven').val() === 'o') {
+//            alert('O Wins!');
+//          } else if ($('.two').val() === 'o' && $('.five').val() === 'o' && $('.eight').val() === 'o') {
+//            alert('O Wins!');
+//          } else if ($('.three').val() === 'o' && $('.six').val() === 'o' && $('.nine').val() === 'o') {
+//            alert('O Wins!');
+//          } else if ($('.one').val() === 'o' && $('.five').val() === 'o' && $('.nine').val() === 'o') {
+//            alert('O Wins!');
+//          } else if ($('.three').val() === 'o' && $('.five').val() === 'o' && $('.seven').val() === 'o') {
+//            alert('O Wins!');
+//          }
+//        });
+
+
+       // Probably better to somehow incorporate an array here instead of this boolean
+       // Refactor later
 
 
       //BELOW THIS IS WHERE FUNCTIONS ARE TESTED
 // ============================================================================================================
 
+$(document).ready(function() {
 var board = 0;
    $('.row').find('.box').click(function() {
          if ($(this).hasClass('O') || $(this).hasClass('X')) {
@@ -42,62 +82,91 @@ var board = 0;
          }
          board++;
 
+         // console.log("code is working !")
 
-  // console.log("code is working !")
+         // function that checks if X is the winner
+         if ($('.one').val() === 'x' && $('.two').val() === 'x' && $('.three').val() === 'x') {
+           alert('X Wins!');
+           endGame();
 
-  // function that checks if X is the winner
-  if ($('.one').val() === 'x' && $('.two').val() === 'x' && $('.three').val() === 'x') {
-       alert('X Wins!');
-     } else if ($('.four').val() === 'x' && $('.five').val() === 'x' && $('.six').val() === 'x') {
-       alert('X Wins!');
-     } else if ($('.seven').val() === 'x' && $('.eight').val() === 'x' && $('.nine').val() === 'x') {
-       alert('X Wins!');
-     } else if ($('.one').val() === 'x' && $('.four').val() === 'x' && $('.seven').val() === 'x') {
-       alert('X Wins!');
-     } else if ($('.two').val() === 'x' && $('.five').val() === 'x' && $('.eight').val() === 'x') {
-       alert('X Wins!');
-     } else if ($('.three').val() === 'x' && $('.six').val() === 'x' && $('.nine').val() === 'x') {
-       alert('X Wins!');
-     } else if ($('.one').val() === 'x' && $('.five').val() === 'x' && $('.nine').val() === 'x') {
-       alert('X Wins!');
-     } else if ($('.three').val() === 'x' && $('.five').val() === 'x' && $('.seven').val() === 'x') {
-       alert('X Wins!');
-     }
-     //function that checks if O is the winner
-     else if ($('.one').val() === 'o' && $('.two').val() === 'o' && $('.three').val() === 'o') {
-       alert('O Wins!');
-     } else if ($('.four').val() === 'o' && $('.five').val() === 'o' && $('.six').val() === 'o') {
-       alert('O Wins!');
-     } else if ($('.seven').val() === 'o' && $('.eight').val() === 'o' && $('.nine').val() === 'o') {
-       alert('O Wins!');
-     } else if ($('.one').val() === 'o' && $('.four').val() === 'o' && $('.seven').val() === 'o') {
-       alert('O Wins!');
-     } else if ($('.two').val() === 'o' && $('.five').val() === 'o' && $('.eight').val() === 'o') {
-       alert('O Wins!');
-     } else if ($('.three').val() === 'o' && $('.six').val() === 'o' && $('.nine').val() === 'o') {
-       alert('O Wins!');
-     } else if ($('.one').val() === 'o' && $('.five').val() === 'o' && $('.nine').val() === 'o') {
-       alert('O Wins!');
-     } else if ($('.three').val() === 'o' && $('.five').val() === 'o' && $('.seven').val() === 'o') {
-       alert('O Wins!');
-     }
-});
+         } else if ($('.four').val() === 'x' && $('.five').val() === 'x' && $('.six').val() === 'x') {
+           alert('X Wins!');
+         } else if ($('.seven').val() === 'x' && $('.eight').val() === 'x' && $('.nine').val() === 'x') {
+           alert('X Wins!');
+         } else if ($('.one').val() === 'x' && $('.four').val() === 'x' && $('.seven').val() === 'x') {
+           alert('X Wins!');
+         } else if ($('.two').val() === 'x' && $('.five').val() === 'x' && $('.eight').val() === 'x') {
+           alert('X Wins!');
+         } else if ($('.three').val() === 'x' && $('.six').val() === 'x' && $('.nine').val() === 'x') {
+           alert('X Wins!');
+         } else if ($('.one').val() === 'x' && $('.five').val() === 'x' && $('.nine').val() === 'x') {
+           alert('X Wins!');
+         } else if ($('.three').val() === 'x' && $('.five').val() === 'x' && $('.seven').val() === 'x') {
+           alert('X Wins!');
+         }
+         //function that checks if O is the winner
+         else if ($('.one').val() === 'o' && $('.two').val() === 'o' && $('.three').val() === 'o') {
+           alert('O Wins!');
+         } else if ($('.four').val() === 'o' && $('.five').val() === 'o' && $('.six').val() === 'o') {
+           alert('O Wins!');
+         } else if ($('.seven').val() === 'o' && $('.eight').val() === 'o' && $('.nine').val() === 'o') {
+           alert('O Wins!');
+         } else if ($('.one').val() === 'o' && $('.four').val() === 'o' && $('.seven').val() === 'o') {
+           alert('O Wins!');
+         } else if ($('.two').val() === 'o' && $('.five').val() === 'o' && $('.eight').val() === 'o') {
+           alert('O Wins!');
+         } else if ($('.three').val() === 'o' && $('.six').val() === 'o' && $('.nine').val() === 'o') {
+           alert('O Wins!');
+         } else if ($('.one').val() === 'o' && $('.five').val() === 'o' && $('.nine').val() === 'o') {
+           alert('O Wins!');
+         } else if ($('.three').val() === 'o' && $('.five').val() === 'o' && $('.seven').val() === 'o') {
+           alert('O Wins!');
+         }
+       });
 
-// $('.btn').click(function(){
-//         $('#board')[0].reset();
-//   });
-
-//function that alerts players once all spaces have been filled
-
-
-// function checkWin () {
-//   if ( )
+       function endGame() {
+         //Remove all remaining cards from here (Clean up the DIV's if need be)
+        document.getElementByClass().innerHTML = "";
+        //document.getElementById(cards).innerHTML = "";
+        //document.getElementById(card).innerHTML = "";
+       }
 //
-//
-//
-// }
+       $(".btn").on("click", function clear(event){
+          $("#board").html(board_table);
+
+})
+       // $('.btn').click(function(){
+       //         $('#board')[0].reset();
+       //   });
+
+       //function that alerts players once all spaces have been filled
 
 
+
+
+       // function checkWin () {
+       //   if ( )
+       //
+       //
+       //
+       // }
+
+
+       //reset button ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+       function initNewGame(){
+           // setup game vars and cards
+       }
+
+       function endGame(){
+           // clean game vars and remove cards
+       }
+
+       function newOrReset(){
+           endGame();
+           initNewGame();
+       }
+       //reset button ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
         // $('box').click( function check(e){
